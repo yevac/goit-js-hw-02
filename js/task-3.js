e940c18bbcf9ae3c5dc23643afaadf7f509db07f
yevac <yevaluation@gmail.com>
2025-08-16 17:22:10 +0300

    hw2

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+const bannedWord = "spam";
+const bannedWordS = "sale";
+
+function checkForSpam(message) {
+    const normalizeMessage = message.toLowerCase();
+    return normalizeMessage.includes(bannedWord) || normalizeMessage.includes(bannedWordS);
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
